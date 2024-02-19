618778703eac5c9290677849b4ad0fa11ba72e3a
Special1626 <itursunaliyev2609200@gmail.com>
2024-02-19 15:41:40 +0500

    commit

---
+++ app/src/main/java/com/example/splashscreentutorial/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.splashscreentutorial
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.splashscreentutorial.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ivNote.alpha = 0f
+        binding.ivNote.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+
+    }
+}
